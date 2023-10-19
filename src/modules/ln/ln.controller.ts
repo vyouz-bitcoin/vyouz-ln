9aef8411cb599dab31f5f7832a3f52c8a845230d
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-10-19 01:00:06 +0100

    feat: added sats and usd value services

--- src/modules/ln/ln.controller.ts
+++ src/modules/ln/ln.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Query, Res, Get } from '@nestjs/common';
 import { LnService } from './ln.service';
 import { InvoiceDto } from './dto/invoice.dto';
 import { Response } from 'express';
@@ -13,6 +13,30 @@ export class LnController {
       res.status(200).json(result);
     } catch (error) {
       res.status(500).json({ message: error.message });
+    }
+  }
+
+  @Get('sats')
+  async getSatsValue(
+    @Query('currency') currency: string,
+    @Query('amount') amount: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const response = await this.lnService.getSatsValue({ amount, currency });
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+
+  @Get('usd')
+  async getUsdValue(@Query('amount') amount: string, @Res() res: Response) {
+    try {
+      const response = await this.lnService.getUsdValue(amount);
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
     }
   }
 }
