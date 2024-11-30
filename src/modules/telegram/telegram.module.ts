ec7e0b2e3fb5db744129922827ac29ebeaaf3fb1
Tobi Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2024-11-30 14:08:07 +0000

    feat: added telegram controller

--- src/modules/telegram/telegram.module.ts
+++ src/modules/telegram/telegram.module.ts
@@ -5,9 +5,11 @@ import { LnService } from '../ln/ln.service';
 import { LnModule } from '../ln/ln.module';
 import { InvoiceGateway } from '../ln/ln.gateway';
 import { ClientManagerService } from '../ln/client-manager.service';
+import { TelegramController } from './telegram.controller';
 
 @Module({
   imports: [ScheduleModule.forRoot(), forwardRef(() => LnModule)],
   providers: [LnService, InvoiceGateway, ClientManagerService],
+  controllers: [TelegramController],
 })
 export class TelegramModule {}
